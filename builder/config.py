72dfd4065805ec1e771058e666a1bba5e5e7baac
HAIL Lab @ Sogang <haillab.sogang@gmail.com>
2025-06-11 23:42:08 +0900

    Update config.py

--- builder/config.py
+++ builder/config.py
@@ -2,6 +2,6 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BUILD_PATH = os.path.join(BASE_PATH, 'docs')
-API_KEY = os.getenv('INPUT_API_KEY', '') or 'AIzaSyC6dzKyPLK2TsfjiWtxytUE7LQEr64QPwc'
+API_KEY = os.getenv('INPUT_API_KEY', '') or 'AIzaSyA9_AkhjKlxNuPfapTJoR4FQRyYU_bKEvY'
 
-DATA_URL = 'https://docs.google.com/spreadsheets/d/1xn6YEWFy4t2v1svjEVmhkUVTfTJesfJtIzNBNZm7tEM/'
+DATA_URL = 'https://docs.google.com/spreadsheets/d/1d6JnxAHQg8DLUq7Jh4lB5dvcH8Exy7duHRTXK6MTf94'
